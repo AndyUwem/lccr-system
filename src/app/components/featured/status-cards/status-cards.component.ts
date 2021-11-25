2117beb90f1130db6d5720c4b45305176d8f1c2f
unknown <essienandy1993@gmail.com>
2021-11-25 05:00:15 +0100

    add greetings message and time functionality

--- src/app/components/featured/status-cards/status-cards.component.ts
+++ src/app/components/featured/status-cards/status-cards.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { CustomerService } from 'src/app/service/customers.service';
 
 @Component({
   selector: 'app-status-cards',
   templateUrl: './status-cards.component.html',
-  styleUrls: ['./status-cards.component.css']
+  styleUrls: ['./status-cards.component.css'],
+  providers: [CustomerService],
 })
 export class StatusCardsComponent implements OnInit {
+  customers: any = [];
+  customerCount: number = 0;
 
-  constructor() { }
+  clock: string = '';
+  attendantName: string = 'BobCat'
+
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
+    this.getCustomersCounts();
+
+    interval(1000).subscribe((t: number) => {
+      this.handleTime();
+    });
   }
 
+  getCustomersCounts() {
+    return this.customerService.findAll().subscribe((customers) => {
+      this.customers = customers;
+      console.log(this.updateCustomersStatus(customers));
+    });
+  }
+
+  updateCustomersStatus(customers: any) {
+    return (this.customerCount = customers.length);
+  }
+
+  handleTime() {
+    let currentClock = new Date(),
+      hours = currentClock.getHours(),
+      minutes = currentClock.getMinutes(),
+      seconds = currentClock.getSeconds(),
+      clockWeather = '',
+      greetings = '';
+
+    if (hours > 11) {
+      clockWeather = 'PM';
+      greetings = `Good Afternoon! ${this.attendantName}`;
+    } else {
+      clockWeather = 'AM';
+      greetings = `Good Morning! ${this.attendantName}`;
+    };
+
+    return (this.clock = `${greetings}      ( ${hours}:${minutes}:${seconds} ${clockWeather} )`);
+  }
 }
--- src/app/components/featured/status-cards/status-cards.module.ts
+++ src/app/components/featured/status-cards/status-cards.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { StatusCardComponent } from './status-card/status-card.component';
 import { StatusCardsComponent } from './status-cards.component';
@@ -5,6 +6,6 @@ import { StatusCardsComponent } from './status-cards.component';
 
 @NgModule({
   declarations: [StatusCardsComponent, StatusCardComponent ],
-  exports: [StatusCardsComponent, StatusCardComponent ],
+  exports: [HttpClientModule, StatusCardsComponent, StatusCardComponent ],
 })
 export class StatusCardsModule {}
