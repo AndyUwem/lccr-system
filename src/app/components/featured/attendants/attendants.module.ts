4179bbab25462adafa81a97c76c0930e049d7b4d
Andy Uwem <essienandy1993@gmail.com>
2021-12-03 02:02:41 +0100

    refactor git repo

--- src/app/components/featured/attendants/attendants.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AttendantComponent } from './attendant/attendant.component';
-import { AttendantsComponent } from './attendants.component';
-
-@NgModule({
-  declarations: [AttendantComponent, AttendantsComponent],
-  exports: [],
-})
-export class AttendantsModule {}
